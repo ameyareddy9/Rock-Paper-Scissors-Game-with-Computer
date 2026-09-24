7db01b02b00c28f5020fa7e7dffb980a444ea597
ameyareddy9 <ameyareddy9@gmail.com>
2026-09-24 18:33:46 +0000

    Add a playGame function to play five rounds and show results

--- script.js
+++ script.js
@@ -36,7 +36,41 @@ function playRound(humanChoice, computerChoice) {
     }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+function playGame () {
+    let humanScore = 0
+    let computerScore = 0
 
-playRound(humanSelection, computerSelection);
+    function playRound(humanChoice, computerChoice) {
+        humanChoice = humanChoice.toLowerCase()
+
+        if(humanChoice !== computerChoice) {
+            const h = humanChoice
+            const c = computerChoice
+
+            if(h === "rock") {
+                if(c === "scissors") humanScore++
+                else computerScore++
+            }
+            else if(h === "scissors") {
+                if(c === "paper") humanScore++
+                else computerScore++
+            }
+            else {
+                if(c === "rock") humanScore++
+                else computerScore++
+            }
+        }
+    }
+
+    let T = 5
+    while(T--) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+        alert(`Round ${5-T}'s results\nYou chose ${humanSelection.toLowerCase()} and computer chose ${computerSelection}\nYour score = ${humanScore} and computer score = ${computerScore}`)
+    }
+    
+    alert(`FINAL SCORE AFTER FIVE ROUNDS:\nYour score: ${humanScore} Computer score: ${computerScore}`)
+}
+
+playGame()
